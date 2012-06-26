6662e05831d5001c0deb53f9fac3ee458bf1bbff
Tyler Whitehouse <tywhitehouse10@hotmail.com>
2012-06-26 15:59:10 -0400

    Added Sprite Sheet support

---
+++ animatedSprite.js
@@ -0,0 +1,55 @@
+function AnimatedSprite(config){
+    
+    // set Defaults
+    this.fps = 1;
+    
+    this.frameCount = 1;
+    
+    this.currentFrame = 0;
+    
+    this.image;
+    
+    this.sourceX = 0;
+    this.sourceY = 0;
+    
+    // if configuration properties are supplied
+    if (config!=null){
+        for (var i in config){
+            this[i] = config[i];
+        }
+    }
+    
+    this.frameWidth = this.image.width / this.frameCount;
+    this.frameHeight= this.image.height;
+    
+    
+    
+    this.stepSprite = function(sender){
+        sender.sourceX = sender.frameWidth * sender.currentFrame;
+        sender.currentFrame++;
+        sender.currentFrame = sender.currentFrame % sender.frameCount;
+    }
+    
+    this.stepSprite(this);
+    this.t = setInterval(this.stepSprite, 1000/this.fps, this);
+    
+    this.stopAnimation = function(){
+        clearInterval(this.t);
+        this.t = null;
+    }
+    
+    this.restartAnimation = function(){
+        this.stopAnimation();
+        this.t = setInterval(this.stepSprite, 1000/this.fps, this);
+    }
+    
+    this.resetFPS = function(newFPS){
+        this.fps = newFPS;
+        this.restartAnimation();
+    }
+    
+    this.drawMe = function(x,y,width,height){
+        ctx.drawImage(this.image, this.sourceX, this.sourceY, this.frameWidth, this.frameHeight, x, y, width, height);
+    }
+    
+}
